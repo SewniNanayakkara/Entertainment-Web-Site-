d4231e7479f276c471c4796e9832af9dff33c0f3
Sewni Nanayakkara <157307421+SewniNanayakkara@users.noreply.github.com>
2024-06-02 12:19:09 +0530

    Initial Commit

---
+++ Entertainment web site/javascript/gallery.js
@@ -0,0 +1,73 @@
+const searchButton = document.querySelector("nav .primary-nav .link-search");
+const closeButton = document.querySelector(".search-container .link-close");
+const PrimaryNav = document.querySelector(".primary-nav");
+const searchContainer = document.querySelector(".search-container");
+const overlay = document.querySelector(".overlay");
+
+searchButton.addEventListener("click", () => {
+    PrimaryNav.classList.add("hide");
+    searchContainer.classList.remove("hide");
+    overlay.classList.add("show");
+})
+
+closeButton.addEventListener("click", () => {
+    PrimaryNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+overlay.addEventListener("click", () => {
+    PrimaryNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+
+// Get the container element
+var btnContainer = document.getElementById("myDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+// search
+
+function Search() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('searchbar');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("Searchresults");
+    li = ul.getElementsByTagName('li');
+
+    if(input.value.length == 0){
+        ul.style.display = "none";
+        return;
+    }else{
+        ul.style.display = "block";
+    }
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+            
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+const bgchange = (id) => {
+    document.body.style.background =document.getElementById(id).innerHTML
+}
